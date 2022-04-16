ce8302c90e82f161166ca29f503df4882e77052f
developermind777 <codingmindpvt@gmail.com>
2022-04-17 00:35:05 +0530

    how to connect mysql in nodejs

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 var express=require('express');
 var app = express();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.set('view engine','ejs');
+
+var conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'nodejs',
+});
+
+//check connection
+conn.connect(function(err)
+{
+    if(err) throw err;
+    console.log("connection successfully!!")
+})
 
 app.get('/',function(req,res)
 {
-    res.send("<h1>Hello world!</h1>");
+    // res.send("<h1>Hello world!</h1>");
+    res.render('insert');
+});
+
+app.post('/insert',function(req,res)
+{
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+    var sql = `insert into users(user_name,user_email,user_password) values('${name}','${email}','${password}')`;
+    conn.query(sql,function(err,results)
+    {
+        if(err) throw err;
+        res.send("<h1>Data send</h1>")
+        });
 });
 
 var server = app.listen(4000,function()
